cb0ae25abcceda31b3c148e3f3e8f9ae2ac6ae53
Nuno Adrego <nunoadrego.geral@gmail.com>
2017-10-04 10:00:00 +0100

    Creation of rs32 header file and updated writenoncanonical

---
+++ src/rs32.h
@@ -0,0 +1,12 @@
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define MAX_FRAME 512
+#define CTRL_SET 0x03
+#define CTRL_DISC 0x0B
+#define CTRL_UA 0x07
+#define CTRL_RR 0x05
+#define CTRL_REJ 0x01
